e91ddd5a7b54e1e3d4295f99ba5af2920427a4c6
Shaik Ali <shaikali99086@gmail.com>
2024-06-29 02:56:46 -0700

    yes

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from pymongo import MongoClient
 
 app = Flask(__name__)
-app.secret_key = "sup"  # More secure secret key
+app.secret_key = "sup"
 
-# Database connection
 connection_string = "mongodb+srv://Stravecodes:ASh2LeaVounah7pu@strave.3nqbbea.mongodb.net/?retryWrites=true&w=majority&appName=Strave"
 mongo = MongoClient(connection_string)
 db = mongo["blaze"]
